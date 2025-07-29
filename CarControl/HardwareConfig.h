e0be48841354d650a1ac10289a071dc30ba913a0
codrutzuuu <codrutmihailaza@gmail.com>
2025-07-29 14:30:24 +0200

    Update hardware config

--- CarControl/HardwareConfig.h
+++ CarControl/HardwareConfig.h
@@ -7,25 +7,27 @@
 
 #include "Arduino.h"
 
-#define MOTOR_LEFT_DIR 13
-#define MOTOR_RIGHT_DIR 12
+#define MOTOR_LEFT_DIR A0
+#define MOTOR_RIGHT_DIR A1
 
-#define MOTOR_LEFT_BREAK 10
-#define MOTOR_RIGHT_BREAK 11
+#define MOTOR_BREAK 6
 
-#define MOTOR_RF_PWM 6
-#define MOTOR_RB_PWM 9
+#define MOTOR_RF_PWM A2
+#define MOTOR_RB_PWM 5
 #define MOTOR_LB_PWM 3
-#define MOTOR_LF_PWM 5
+#define MOTOR_LF_PWM A5
 const unsigned int MOTORS_PWM[] = {MOTOR_RF_PWM, MOTOR_RB_PWM, MOTOR_LB_PWM, MOTOR_LF_PWM};
 
-#define MOTOR_LEFT_SPEED ???
-#define MOTOR_RIGHT_SPEED ???
+#define MOTOR_RF_SPEED A3
+#define MOTOR_RB_SPEED 4
+#define MOTOR_LB_SPEED 2
+#define MOTOR_RB_SPEED A4
+
 
 #define RIGHT_DIR_DEFAULT HIGH
 #define LEFT_DIR_DEFAULT LOW
 
 #define TICKS_PER_REVOLUTION 98
-#define WHEEL_RADIUS 8 //cm
+#define WHEEL_RADIUS 14S
 
 #endif

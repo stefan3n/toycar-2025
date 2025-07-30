127183e876e301f7dd4b65b845921e74f4fd6bf3
Mihaila Gabriel-Codrut (uih38915) <gabriel-codrut.mihaila@continental-corporation.com>
2025-07-30 15:02:55 +0300

    orice

--- JetsonControl/main/m.py
+++
@@ -1,7 +0,0 @@
-import serial
-try:
-    ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
-    print("Port opened successfully")
-    ser.close()
-except Exception as e:
-    print(f"Error opening serial port: {e}")

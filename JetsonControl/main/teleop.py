996b363b16a8b94787eb25b34bb53fb8ed2da853
Mihaila Gabriel-Codrut (uih38915) <gabriel-codrut.mihaila@continental-corporation.com>
2025-07-30 09:51:30 +0300

    teleop_update

--- JetsonControl/main/teleop.py
+++ JetsonControl/main/teleop.py
@@ -1,39 +1,43 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-
 import sys
-import termios
 import tty
+import termios
 import select
 
 class KeyboardTeleop(Node):
     def __init__(self):
         super().__init__('keyboard_teleop')
         self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
-        self.get_logger().info('Teleop ready. W/A/S/D to move. Q to quit.')
+        self.get_logger().info('Use arrow keys to move. Press q to quit.')
+
         self.settings = termios.tcgetattr(sys.stdin)
         tty.setcbreak(sys.stdin.fileno())
         self.timer = self.create_timer(0.1, self.timer_callback)
 
     def timer_callback(self):
-        if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+        if select.select([sys.stdin], [], [], 0)[0]:
             key = sys.stdin.read(1)
             twist = Twist()
 
-            if key == 'w':
-                twist.linear.x = 1.0
-            elif key == 's':
-                twist.linear.x = -1.0
-            elif key == 'a':
-                twist.angular.z = 1.0
-            elif key == 'd':
-                twist.angular.z = -1.0
-            elif key == 'q':
-                self.get_logger().info("Quit pressed. Exiting...")
+            if key == 'q':
+                self.get_logger().info('Exiting...')
                 termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
                 rclpy.shutdown()
                 return
+
+            elif key == '\x1b':
+                if sys.stdin.read(1) == '[':
+                    direction = sys.stdin.read(1)
+                    if direction == 'A':
+                        twist.linear.x = 1.0
+                    elif direction == 'B':
+                        twist.linear.x = -1.0
+                    elif direction == 'C':
+                        twist.angular.z = -1.0
+                    elif direction == 'D':
+                        twist.angular.z = 1.0
 
             self.publisher_.publish(twist)
 

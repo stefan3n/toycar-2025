6dd2a135719a404f593e9f59b073c21dd9c7511b
Mihaila Gabriel-Codrut (uih38915) <gabriel-codrut.mihaila@continental-corporation.com>
2025-07-30 08:51:13 +0300

    teleop

--- JetsonControl/main/teleop.py
+++ JetsonControl/main/teleop.py
@@ -48,3 +48,65 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+import serial
+
+
+class CmdVelListener(Node):
+    def __init__(self):
+        super().__init__('cmd_vel_listener')
+        self.subscription = self.create_subscription(
+            Twist,
+            '/cmd_vel',
+            self.listener_callback,
+            10)
+        self.serial_port = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+        self.pwm_max = 30
+        self.pwm_min = 20
+
+    def listener_callback(self, msg):
+        linear = msg.linear.x
+        angular = msg.angular.z
+
+        pwmL, pwmR = 0, 0
+        dirL, dirR = 1, 1
+
+        def cap_pwm(val):
+            val = int(abs(val))
+            return max(self.pwm_min, min(self.pwm_max, val))
+
+        if abs(linear) > 0.1:
+            pwmL = pwmR = cap_pwm(linear * 100)
+            dirL = dirR = 1 if linear > 0 else 0
+
+        elif abs(angular) > 0.1:
+            pwm = cap_pwm(angular * 100)
+            pwmL = pwmR = pwm
+            if angular > 0:
+                dirL = 1
+                dirR = 0  
+            else:
+                dirL = 0
+                dirR = 1  
+
+        else:
+            pwmL = pwmR = 0
+            dirL = dirR = 1
+
+        cmd = f"<0,2,{pwmR},{pwmL},{dirR},{dirL}>"
+        self.serial_port.write(cmd.encode())
+        self.get_logger().info(f'Trimis la Arduino: {cmd}')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = CmdVelListener()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    node.destroy_node()
+    rclpy.shutdown()
